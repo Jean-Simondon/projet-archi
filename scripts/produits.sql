b486c03223fb782017e745ed7afa869f64ebf381
romain cadarsi <romain.cadarsi@codein.fr>
2021-02-11 17:37:54 +0100

    FIX Script sql

--- scripts/produits.sql
+++ scripts/produits.sql
@@ -1,8 +1,10 @@
-CREATE TABLE Produits(id NUMBER, nom VARCHAR(50), prixUnitaireHT DOUBLE PRECISION, tauxTVA DOUBLE PRECISION,
+DROP TABLE Produits;
+
+CREATE TABLE Produits(id NUMBER, nom VARCHAR(50), qte NUMBER, prixUnitaireHT DOUBLE PRECISION,
                       CONSTRAINT pk_produit PRIMARY KEY (id),
                       CONSTRAINT nn_nom CHECK (nom IS NOT NULL),
-                      CONSTRAINT prixUnitaireHT CHECK (prixUnitaireHT IS NOT NULL),
-                      CONSTRAINT tauxTVA CHECK (tauxTVA IS NOT NULL)
+                      CONSTRAINT nn_prixUnitaireHT CHECK (prixUnitaireHT IS NOT NULL),
+                      CONSTRAINT nn_qte CHECK (qte IS NOT NULL)
 ) ;
 
 
@@ -10,7 +12,7 @@ CREATE SEQUENCE seqProduit
     START WITH 1 INCREMENT BY 1;
 
 
-CREATE OR REPLACE PROCEDURE insertProduit(p_nom IN Produits.nom%TYPE, p_prixUnitaireHT IN Produits.prixUnitaireHT%TYPE, p_tauxTVA IN Produits.tauxTVA%TYPE) IS
+CREATE OR REPLACE PROCEDURE insertProduit(p_nom IN Produits.nom%TYPE, p_prixUnitaireHT IN Produits.prixUnitaireHT%TYPE, p_qte IN Produits.qte%TYPE) IS
 BEGIN
-INSERT INTO Produits VALUES (seqProduit.NEXTVAL, p_nom, p_prixUnitaireHT, p_tauxTVA) ;
+INSERT INTO Produits VALUES (seqProduit.NEXTVAL, p_nom, p_prixUnitaireHT, p_qte) ;
 END;
