bda9841223e6c86857fc7af98aa753ad2120344e
Simondon Jean <jean@lajungle.fr>
2021-02-11 16:45:43 +0100

    Controller Achat Vente

--- src/controller/ControllerAchatVente.java
+++ src/controller/ControllerAchatVente.java
@@ -1,13 +1,24 @@
 package controller;
 
+import metier.Catalogue;
+
 public class ControllerAchatVente {
 
-//    public String[] getNomsProduits(){}
+    private static Catalogue cat = new Catalogue();
 
-//    public boolean isPositiveNumber() {};
+    public static String[] getNomsProduits()
+    {
+        return ControllerAchatVente.cat.getNomProduits();
+    }
 
-//    public boolean acheterProduit() {};
+    public static boolean acheterProduit(String nomProduit, int qteAchetee)
+    {
+        return ControllerAchatVente.cat.acheterStock(nomProduit, qteAchetee);
+    };
 
-//    public boolean vendreProduit() {};
+    public static boolean vendreProduit(String nomProduit, int qteAchetee)
+    {
+        return ControllerAchatVente.cat.vendreStock(nomProduit, qteAchetee);
+    };
 
 }
